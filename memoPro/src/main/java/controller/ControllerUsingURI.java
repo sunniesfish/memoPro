eeeee16b5c4cf77d3c31ebf20f4c360a0a1dfb7d
user <user@jyhan>
2024-03-04 18:26:17 +0900

    *create ControllerUsingURI CommandHandler NullHandler commandHandler.properties
    *change
    directory

---
+++ memoPro/src/main/java/controller/ControllerUsingURI.java
@@ -0,0 +1,99 @@
+package controller;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import command.CommandHandler;
+
+
+@WebServlet(
+		urlPatterns = { 
+				"/ControllerUsingURI", 
+				"*.do"
+		}, 
+		initParams = { 
+				@WebInitParam(name = "configFile", value = "../config/commandHandler.properties")
+		})
+public class ControllerUsingURI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private Map<String, CommandHandler> commandHandlerMap = new HashMap<>();
+	
+	public void init(ServletConfig config) throws ServletException {
+		String configFile = getInitParameter("configFile");
+		Properties prop = new Properties();
+		String configFilePath = getServletContext().getRealPath(configFile);
+		try (FileReader fis = new FileReader(configFilePath)) {
+			prop.load(fis);
+		} catch (IOException e) {
+			throw new ServletException(e);
+		}
+		Iterator keyIter = prop.keySet().iterator();
+		while (keyIter.hasNext()) {
+			String command = (String) keyIter.next();
+			String handlerClassName = prop.getProperty(command);
+			try {
+				Class<?> handlerClass = Class.forName(handlerClassName);
+				CommandHandler handlerInstance = null;
+				try {
+					handlerInstance = (CommandHandler) handlerClass.getDeclaredConstructor().newInstance();
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				} catch (NoSuchMethodException e) {
+					e.printStackTrace();
+				} catch (SecurityException e) {
+					e.printStackTrace();
+				}
+				commandHandlerMap.put(command, handlerInstance);
+			} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+				throw new ServletException(e);
+			}
+		}
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+	
+	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String command = request.getRequestURI();
+		if(command.indexOf(request.getContextPath())==0) { 
+			command = command.substring(request.getContextPath().length());
+		}
+		CommandHandler handler = commandHandlerMap.get(command);
+		if(handler == null) {
+			handler = new NullHandler();
+		}
+		String viewPage = null;
+		try {
+			viewPage = handler.process(request, response);
+		} catch (Throwable e) {
+			throw new ServletException(e);
+		}
+		if(viewPage != null) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+			dispatcher.forward(request, response);
+		}
+	}
+
+
+}
---
+++ memoPro/src/main/java/controller/NullHandler.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import command.CommandHandler;
+
+public class NullHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		res.sendError(HttpServletResponse.SC_NOT_FOUND);
+		return null;
+	}
+
+}
